426100886394896d00b1824a091c7e870df629e9
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-08-04 22:22:58 +0300

    feat: header in process

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,19 +7,24 @@ import headerLogoAccount from "../../images/logo-account.svg";
 
 function Header() {
   const location = useLocation();
-  const isLocationSignIn = location.pathname === '/sign-in';
   const isLocationMain = location.pathname === '/';
   const [isColumnMenu, setColumnMenu] = React.useState(false);
-  const [isLoggedIn, setLoggedIn] = React.useState(false);
   const history = useHistory();
 
- function handleLogOut() {
-    setColumnMenu(false);
-  }
 
   function handleSignIn() {
-    history.push('/sign-in');
   }
+
+  function handleMovies() {
+  }
+
+  function handleSavedMovies() {
+  }
+
+  function handleMainPage() {
+    history.push('/');
+  }
+
 
   function handleMenuClick() {
     setColumnMenu(true);
@@ -29,29 +34,27 @@ function Header() {
     setColumnMenu(false);
   }
 
-  React.useEffect(()=>{
-    setColumnMenu(false);
-    setLoggedIn(false);
-  },[])
-
   return (
-    <header className={`header  ${!isColumnMenu ? "page__container" : ""} ${isColumnMenu ? "header_columned" : ""} ${isLocationMain ? "page_place_main-top" : "header__place_not-main"}`}>
+    <header className={`header  ${!isColumnMenu ? "page__container" : ""} ${isLocationMain ? "page_place_main-top" : "header__place_not-main"}`}>
       <img
         className="header__logo appear"
         src={headerLogo}
         alt="Логотип сайта"
+        onClick={handleMainPage}
       />
-      <nav className={`header__nav-container appear ${isLoggedIn && !isColumnMenu ? "header__nav-container_type_inactive" : ""} ${isColumnMenu ? "header__nav-container_type_column" : ""} `}>
+      <nav className={`header__nav-container appear ${(!isColumnMenu && !isLocationMain) ? "header__nav-container_type_inactive" : ""} ${isColumnMenu ? "header__nav-container_type_column" : ""} `}>
           {isLocationMain ?
           <div className="header__auth-container">
-            <NavLink className="header__nav-item" to={"/sign-up"}>{!isLoggedIn ? "Регистрация" : ""}</NavLink>
-            <NavLink onClick={handleSignIn} className="header__nav-item header__nav-item_type_login" to={"/sign-in"}>{!isLoggedIn ? "Войти" : ""}</NavLink>
+            <NavLink className="header__nav-item" to={"/sign-up"}>Регистрация</NavLink>
+            <NavLink onClick={handleSignIn} className="header__nav-item header__nav-item_type_login" to={"/sign-in"}>Войти</NavLink>
           </div>
           :
-          <div className="header__container">
-            <div className="header__films-container">
-              <NavLink onClick={!isLoggedIn ? handleSignIn : handleLogOut} className={`header__nav-item ${isLocationMain ? "header__nav-item_active" : ""}`} to={"/movies"}>Фильмы</NavLink>
-              <NavLink className="header__nav-item" to={"/saved-movies"}>Сохраненные фильмы</NavLink>
+          <div className={`header__container ${isColumnMenu ? "header__container_type_column" : ""}`}>
+            <button onClick={handleCloseMenu} type="button" aria-label="Закрыть меню" className={`btn-close btn-close_place_header ${isColumnMenu ? "btn-close_active" : ""}`}></button>
+            <div className={`header__films-container ${isColumnMenu ? "header__films-container_type_column" : ""}`}>
+              <NavLink onClick={handleMainPage} className={`header__nav-item ${!isColumnMenu ? "header__nav-item_type_inactive" : ""}`} activeClassName={!isColumnMenu? "header__nav-item_active" : "header__nav-item_active-column"} to={"/"}>Главная</NavLink>
+              <NavLink onClick={handleMovies} className="header__nav-item" activeClassName={!isColumnMenu? "header__nav-item_active" : "header__nav-item_active-column"} to={"/movies"}>Фильмы</NavLink>
+              <NavLink onClick={handleSavedMovies} className="header__nav-item" activeClassName={!isColumnMenu? "header__nav-item_active" : "header__nav-item_active-column"} to={"/saved-movies"}>Сохраненные фильмы</NavLink>
             </div>
             <div className="header__account-container">
               <NavLink className="header__nav-item" to={"/profile"}>Аккаунт</NavLink>
@@ -64,12 +67,14 @@ function Header() {
           </div>
           }
       </nav>
-      {/* <div onClick={handleMenuClick} className= {`header__btn-menu ${(!isLoggedIn || isColumnMenu) ? "header__btn-menu_type_inactive" : ""}`}>
-        <div className="header__burger-line"></div>
-        <div className="header__burger-line"></div>
-        <div className="header__burger-line"></div>
-      </div>
-      <button onClick={handleCloseMenu} type="button" aria-label="Закрыть меню" className={`btn-close btn-close_place_header ${isColumnMenu ? "btn-close_active" : ""}`}></button> */}
+      {
+        !isLocationMain &&
+        <div onClick={handleMenuClick} className= {`header__btn-menu ${isLocationMain && isColumnMenu ? "header__btn-menu_type_inactive" : ""}`}>
+          <div className="header__burger-line"></div>
+          <div className="header__burger-line"></div>
+          <div className="header__burger-line"></div>
+        </div>
+      }
 
     </header>
   );
