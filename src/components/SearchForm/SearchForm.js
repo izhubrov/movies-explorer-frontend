f1bcca14ddfd35c6bbd04f5b33765e78e5e4cab8
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-08-16 21:42:48 +0300

    bugfix: set css searchform input focus

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,10 +2,20 @@ import React from "react";
 import "./SearchForm.css";
 
 function SearchForm() {
+  const [isFocused, setFocused] = React.useState(false);
 
   function handleSubmit(evt) {
     evt.preventDefault();
   }
+
+  function handleFocus() {
+    setFocused(true);
+  }
+
+  function handleLeave() {
+    setFocused(false);
+  }
+
 
   return (
     <form
@@ -14,13 +24,15 @@ function SearchForm() {
       onSubmit={handleSubmit}
       noValidate
     >
-      <label className="search-form__field">
+      <label className={`search-form__field ${isFocused ? "search-form__field_focused" : ""}`}>
         <input
           type="text"
           name="film"
           placeholder="Фильм"
           className="search-form__input"
           required
+          onFocus={handleFocus}
+          onBlur={handleLeave}
         />
         <button
           type="submit"
