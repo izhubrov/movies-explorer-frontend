ae44247ec5739e08263ca071795c978792f38812
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-08-25 23:36:43 +0300

    fix: success popup

--- src/components/SuccessPopup/SuccessPopup.js
+++ src/components/SuccessPopup/SuccessPopup.js
@@ -3,7 +3,7 @@ import "./SuccessPopup.css";
 
 function SuccessPopup({ isSuccess }) {
   return (
-    <div className={`success-popup ${isSuccess ? "success-popup_opened" : ""} `}>
+    <div className={`success-popup appear ${isSuccess ? "success-popup_opened" : ""} `}>
       <h2 className="success-popup__title">Данные успешно применены!</h2>
     </div>
   );
